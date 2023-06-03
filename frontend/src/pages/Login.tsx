b0cf06bb67e9666ab041985096790c77ba0ba0bb
NathanHermes <hermesnathan8@gmail.com>
2023-06-03 17:03:58 -0300

    Feat: init Login page

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+export const Login = () => {
+  return (
+    <>
+      <h1 className="w-full font-Inter text-blue-800">Página de login</h1>
+      <p>Testando a página de login</p>
+    </>
+  );
+};
