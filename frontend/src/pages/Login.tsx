bc35c62670bbe76a1a2e38749db4dd5add3ea22d
NathanHermes <hermesnathan8@gmail.com>
2023-06-05 20:51:11 -0300

    Style: rename page

--- frontend/src/pages/Login.tsx
+++
@@ -1,8 +0,0 @@
-export const Login = () => {
-  return (
-    <>
-      <h1 className="w-full font-Inter text-blue-800">Página de login</h1>
-      <p>Testando a página de login</p>
-    </>
-  );
-};
