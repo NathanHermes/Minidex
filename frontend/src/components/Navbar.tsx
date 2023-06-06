d79d77cc9fe1c3f7285ccfc079e057e042976452
NathanHermes <hermesnathan8@gmail.com>
2023-06-05 21:56:48 -0300

    Feat: init structure of navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,29 @@
+import { Link } from "react-router-dom";
+import { RxPlay } from "react-icons/rx";
+import { CgPokemon } from "react-icons/cg";
+
+// import PokeballIcon from "../assets/icon-pokemon.svg";
+
 export const Navbar = () => {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div>
+        <Link to={"/"}>
+          <RxPlay />
+          Jogar TbPokeball
+        </Link>
+
+        <Link to={"/"}>
+          <CgPokemon className="h-10 w-10 text-pokedex-500" />
+          Jogar TbPokeball
+        </Link>
+      </div>
+
+      <div>
+        <Link to={"/"}>
+          Fulano <span>100xp</span>
+        </Link>
+      </div>
+    </nav>
+  );
 };
